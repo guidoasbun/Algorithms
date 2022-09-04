eb1b4e721f121f34c13a0a235545a00ec1a5710f
guidoasbun <guido@asbun.io>
2022-09-04 00:02:52 -0700

    Java Validate Sequence

--- Java/src/ValidSequence/Test.java
+++ Java/src/ValidSequence/Test.java
@@ -1,8 +1,24 @@
 package ValidSequence;
 
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        // Write your code here.
-        System.out.println("Hello, World!");
+
+        List<Integer> array1 = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+        List<Integer> sequence1 = List.of(1, 6, -1, 10);
+
+        // List<Integer> array2 = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+        // List<Integer> sequence2 = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+
+        // List<Integer> array3 = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+        // List<Integer> sequence3 = List.of(5, 1, 22, 25, 6, -1, 8, 10);
+
+        System.out.print("Solution 1: ");
+        System.out.println(ValidSequence.isValidSequence(array1, sequence1));
+
+        System.out.print("Solution 2: ");
+        System.out.println(ValidSequence.isValidSequence2(array1, sequence1));
+
     }
 }
--- Java/src/ValidSequence/ValidSequence.java
+++ Java/src/ValidSequence/ValidSequence.java
@@ -1,4 +1,40 @@
 package ValidSequence;
 
+import java.util.List;
+
 public class ValidSequence {
+
+  public static boolean isValidSequence(List<Integer> array, List<Integer> sequence) {
+    // O(n) time | O(1) space
+    int arrayIndex = 0;
+    int sequenceIndex = 0;
+    int arraySize = array.size();
+    int sequenceSize = sequence.size();
+
+    while (arrayIndex < arraySize && sequenceIndex < sequenceSize) {
+      if (array.get(arrayIndex) == sequence.get(sequenceIndex)) {
+        sequenceIndex++;
+      }
+      arrayIndex++;
+    }
+
+    return sequenceIndex == sequenceSize;
+  }
+
+  public static boolean isValidSequence2(List<Integer> array, List<Integer> sequence) {
+    // O(n) time | O(1) space
+    int sequenceIndex = 0;
+    int sequenceSize = sequence.size();
+
+    for (int value : array) {
+      if (sequenceIndex == sequenceSize) {
+        break;
+      }
+      if (value == sequence.get(sequenceIndex)) {
+        sequenceIndex++;
+      }
+    }
+
+    return sequenceIndex == sequenceSize;
+  }
 }
